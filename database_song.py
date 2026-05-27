f8af6e173d490b0c8990eccf261c276b443fceb0
Ap4tt <appaaatttt2@gmail.com>
2026-05-27 11:48:49 +0700

    Update database_song.py

--- database_song.py
+++ database_song.py
@@ -221,4 +221,394 @@ SONG_DATABASE = [
         "artist": "Metallica",
         "duration": 386
     },
+        {
+        "db_id": 38,
+        "title": "Dead Memories",
+        "artist": "Slipknot",
+        "duration": 269
+    },
+    {
+        "db_id": 39,
+        "title": "I-E-A-I-A-I-O",
+        "artist": "System Of A Down",
+        "duration": 189
+    },
+    {
+        "db_id": 40,
+        "title": "Bat Country",
+        "artist": "Avenged Sevenfold",
+        "duration": 312
+    },
+    {
+        "db_id": 41,
+        "title": "The Final Episode (Let's Change The Channel)",
+        "artist": "Asking Alexandria",
+        "duration": 242
+    },
+    {
+        "db_id": 42,
+        "title": "Papercut",
+        "artist": "Linkin Park",
+        "duration": 185
+    },
+    {
+        "db_id": 43,
+        "title": "Somewhere I Belong",
+        "artist": "Linkin Park",
+        "duration": 214
+    },
+    {
+        "db_id": 44,
+        "title": "Radio/Video",
+        "artist": "System Of A Down",
+        "duration": 249
+    },
+    {
+        "db_id": 45,
+        "title": "Spiders",
+        "artist": "System Of A Down",
+        "duration": 215
+    },
+    {
+        "db_id": 46,
+        "title": "Killpop",
+        "artist": "Slipknot",
+        "duration": 225
+    },
+    {
+        "db_id": 47,
+        "title": "Critical Acclaim",
+        "artist": "Avenged Sevenfold",
+        "duration": 315
+    },
+    {
+        "db_id": 48,
+        "title": "Plug In Baby",
+        "artist": "Muse",
+        "duration": 218
+    },
+    {
+        "db_id": 49,
+        "title": "¡Viva La Gloria!",
+        "artist": "Green Day",
+        "duration": 211
+    },
+    {
+        "db_id": 50,
+        "title": "You Shook Me All Night Long",
+        "artist": "AC/DC",
+        "duration": 210
+    },
+    {
+        "db_id": 51,
+        "title": "American Idiot",
+        "artist": "Green Day",
+        "duration": 176
+    },
+    {
+        "db_id": 52,
+        "title": "Back In Black",
+        "artist": "AC/DC",
+        "duration": 256
+    },
+    {
+        "db_id": 53,
+        "title": "Supermassive Black Hole",
+        "artist": "Muse",
+        "duration": 212
+    },
+    {
+        "db_id": 54,
+        "title": "Courtesy Call",
+        "artist": "Thousand Foot Krutch",
+        "duration": 237
+    },
+    {
+        "db_id": 55,
+        "title": "Left Behind",
+        "artist": "Slipknot",
+        "duration": 241
+    },
+    {
+        "db_id": 56,
+        "title": "Unsainted",
+        "artist": "Slipknot",
+        "duration": 260
+    },
+    {
+        "db_id": 57,
+        "title": "The Heretic Anthem",
+        "artist": "Slipknot",
+        "duration": 254
+    },
+    {
+        "db_id": 58,
+        "title": "Doing Time",
+        "artist": "Avenged Sevenfold",
+        "duration": 206
+    },
+    {
+        "db_id": 59,
+        "title": "Heretic",
+        "artist": "Avenged Sevenfold",
+        "duration": 295
+    },
+    {
+        "db_id": 60,
+        "title": "I Won't Give In",
+        "artist": "Asking Alexandria",
+        "duration": 230
+    },
+    {
+        "db_id": 61,
+        "title": "Can You Feel My Heart",
+        "artist": "Bring Me The Horizon",
+        "duration": 227
+    },
+    {
+        "db_id": 62,
+        "title": "Faded Out (feat. Within Temptation)",
+        "artist": "Asking Alexandria",
+        "duration": 186
+    },
+    {
+        "db_id": 63,
+        "title": "Alone In A Room",
+        "artist": "Asking Alexandria",
+        "duration": 246
+    },
+    {
+        "db_id": 64,
+        "title": "My Demons",
+        "artist": "STARSET",
+        "duration": 288
+    },
+    {
+        "db_id": 65,
+        "title": "Carnivore",
+        "artist": "STARSET",
+        "duration": 263
+    },
+    {
+        "db_id": 66,
+        "title": "Basket Case",
+        "artist": "Green Day",
+        "duration": 182
+    },
+    {
+        "db_id": 67,
+        "title": "Holiday",
+        "artist": "Green Day",
+        "duration": 233
+    },
+    {
+        "db_id": 68,
+        "title": "Hayloft",
+        "artist": "Mother Mother",
+        "duration": 182
+    },
+    {
+        "db_id": 69,
+        "title": "Hayloft II",
+        "artist": "Mother Mother",
+        "duration": 215
+    },
+    {
+        "db_id": 70,
+        "title": "Tears Don't Fall",
+        "artist": "Bullet For My Valentine",
+        "duration": 348
+    },
+    {
+        "db_id": 71,
+        "title": "Throne",
+        "artist": "Bring Me The Horizon",
+        "duration": 192
+    },
+    {
+        "db_id": 72,
+        "title": "Shadow Moses",
+        "artist": "Bring Me The Horizon",
+        "duration": 243
+    },
+    {
+        "db_id": 73,
+        "title": "She's Gone",
+        "artist": "STEELHEART",
+        "duration": 392
+    },
+    {
+        "db_id": 74,
+        "title": "I'll Never Let You Go",
+        "artist": "STEELHEART",
+        "duration": 307
+    },
+    {
+        "db_id": 75,
+        "title": "Steelheart",
+        "artist": "STEELHEART",
+        "duration": 344
+    },
+    {
+        "db_id": 76,
+        "title": "Kingslayer (feat. BABYMETAL)",
+        "artist": "Bring Me The Horizon",
+        "duration": 220
+    },
+    {
+        "db_id": 77,
+        "title": "Dream On",
+        "artist": "Aerosmith",
+        "duration": 267
+    },
+    {
+        "db_id": 78,
+        "title": "Pride (In The Name Of Love)",
+        "artist": "U2",
+        "duration": 229
+    },
+    {
+        "db_id": 79,
+        "title": "I Miss You",
+        "artist": "blink-182",
+        "duration": 227
+    },
+    {
+        "db_id": 80,
+        "title": "Adam's Song",
+        "artist": "blink-182",
+        "duration": 252
+    },
+    {
+        "db_id": 81,
+        "title": "All The Small Things",
+        "artist": "blink-182",
+        "duration": 167
+    },
+    {
+        "db_id": 82,
+        "title": "Stay Together For The Kids",
+        "artist": "blink-182",
+        "duration": 239
+    },
+    {
+        "db_id": 83,
+        "title": "Higher",
+        "artist": "Creed",
+        "duration": 317
+    },
+    {
+        "db_id": 84,
+        "title": "All Apologies",
+        "artist": "Nirvana",
+        "duration": 233
+    },
+    {
+        "db_id": 85,
+        "title": "Heart-Shaped Box",
+        "artist": "Nirvana",
+        "duration": 281
+    },
+    {
+        "db_id": 86,
+        "title": "Rock And Roll All Nite",
+        "artist": "KISS",
+        "duration": 169
+    },
+    {
+        "db_id": 87,
+        "title": "Sweet Victory",
+        "artist": "David Glen Eisley, Bob Kulick",
+        "duration": 231
+    },
+    {
+        "db_id": 88,
+        "title": "Teenagers",
+        "artist": "My Chemical Romance",
+        "duration": 162
+    },
+    {
+        "db_id": 89,
+        "title": "Helena",
+        "artist": "My Chemical Romance",
+        "duration": 205
+    },
+    {
+        "db_id": 90,
+        "title": "Welcome to the Black Parade",
+        "artist": "My Chemical Romance",
+        "duration": 311
+    },
+    {
+        "db_id": 91,
+        "title": "The Great Gig in the Sky",
+        "artist": "Pink Floyd",
+        "duration": 284
+    },
+    {
+        "db_id": 92,
+        "title": "Critical Darling",
+        "artist": "Slipknot",
+        "duration": 386
+    },
+    {
+        "db_id": 93,
+        "title": "Nothing Else Matters",
+        "artist": "Metallica",
+        "duration": 388
+    },
+    {
+        "db_id": 94,
+        "title": "Raised on Rock",
+        "artist": "Scorpions",
+        "duration": 239
+    },
+    {
+        "db_id": 95,
+        "title": "In Trance - Live",
+        "artist": "Scorpions",
+        "duration": 246
+    },
+    {
+        "db_id": 96,
+        "title": "Rhythm of Love",
+        "artist": "Scorpions",
+        "duration": 219
+    },
+    {
+        "db_id": 97,
+        "title": "Monster",
+        "artist": "Skillet",
+        "duration": 178
+    },
+    {
+        "db_id": 98,
+        "title": "Danger Line",
+        "artist": "Avenged Sevenfold",
+        "duration": 328
+    },
+    {
+        "db_id": 99,
+        "title": "Blood & Water",
+        "artist": "Memphis May Fire",
+        "duration": 231
+    },
+    {
+        "db_id": 100,
+        "title": "M.I.A.",
+        "artist": "Avenged Sevenfold",
+        "duration": 526
+    },
+    {
+        "db_id": 101,
+        "title": "Work For The Working Man",
+        "artist": "Bon Jovi",
+        "duration": 244
+    },
+    {
+        "db_id": 102,
+        "title": "Blood On Blood",
+        "artist": "Bon Jovi",
+        "duration": 377
+    }
 ]
