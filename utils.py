8d21baeba9259b86834a941211dd73a442ab436f
Ap4tt <appaaatttt2@gmail.com>
2026-06-06 22:18:06 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,3 @@
-def input_integer(prompt, min_val):
-    while True:
-        try:
-            val = int(input(prompt))
-            if val < min_val:
-                print(f"  [!] Invalid: Angka minimal adalah {min_val}.")
-                continue
-            return val
-        except ValueError:
-            print("  [!] Invalid: Masukkan format angka yang benar.")
-
-def input_string(prompt):
-    while True:
-        val = input(prompt).strip()
-        if not val:
-            print("  [!] Invalid: Input tidak boleh kosong.")
-            continue
-        return val
+def format_duration(seconds):
+    """Mengubah detik menjadi format m:ss"""
+    return f"{seconds // 60}:{seconds % 60:02d}"
