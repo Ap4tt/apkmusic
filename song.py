0e6b3b1496242e7ff292cf4c6b069e1468119265
Ap4tt <appaaatttt2@gmail.com>
2026-06-06 22:18:36 +0700

    Update song.py

--- song.py
+++ song.py
@@ -1,14 +1,8 @@
 class Song:
-    """Class Node untuk Doubly Circular Linked List"""
-    def __init__(self, db_id, title, artist, duration):
-        # Menyimpan referensi ID Database aslinya
-        self.db_id = db_id
-        self.playlist_id = db_id + 1000
-        
-        self.title = title
-        self.artist = artist
+    """Node untuk Doubly Circular Linked List"""
+    def __init__(self, title, artist, duration):
+        self.title    = title
+        self.artist   = artist
         self.duration = duration
-        
-        # Doubly Linked List memiliki 2 arah pointer
-        self.next = None
-        self.prev = None
+        self.next     = None
+        self.prev     = None
